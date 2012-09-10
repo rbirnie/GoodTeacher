2b03a025a2313131ef6c9fe853c7d2111a5c1933
rbirnie <rbirnie@gmail.com>
2012-09-09 23:07:07 -0700

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodTeacher::Application.config.secret_token = 'c1030579296a2c65851d50589de314a7ab97c516b540ac180c1c84d0bf98e5d1c1d3a64fa112e8c680ef5fceb8d5f6a607e5292126f3846e39a4df27ec007719'
